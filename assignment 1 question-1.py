4adfdf13aad18b4f1751b841756625c6caf81acb
bobby9081 <124876400+bobby9081@users.noreply.github.com>
2023-02-10 21:13:09 +0530

    Add files via upload

---
+++ assignment 1 question-1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+fibonacci series between o to 50
+
+
+# In[2]:
+
+
+a=0
+b=1
+for i in range(50):
+    print(a,end=',')
+    c=a+b
+    b=a
+    a=c
+
+
+# In[ ]:
+
+
+
+
